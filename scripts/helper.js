8b70881476bcce6d468b263350af81c05b2ba8a5
mugabodeo <mugabodeo6@gmail.com>
2020-08-13 11:44:31 +0200

    able to save comments

--- scripts/helper.js
+++ scripts/helper.js
@@ -15,47 +15,42 @@ function getLocation(showPosition) {
 }
 
 
-
-
 // authentication scrr__
-var pattern=/^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
 
 // validation patterns
 
-
-
-function contactValidation(){
-    const contactForm= document.querySelector('#contactForm');
-    const contactError=document.getElementById("contactError");
+const contactForm= document.querySelector('#contactForm');
+const pattern=/^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+contactForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    e.stopPropagation();
     const email= contactForm['email'].value;
     const name=contactForm['name'].value;
-    const subject=contactForm['subject'].value
-    
-    contactForm.addEventListener('submit',(e)=>{
-        if(email.match(pattern)){
-            contactError.innerHTML="success";
-            db.collection('queries').add({
-                email:email,
-                name:name,
-                subject:subject
-            }).then(success =>{
-                console.log('success')
-            }).catch(err=>{
-                console.log('error')
-            })
-        }
+    const subject=contactForm['subject'].value;
+    const contactError=document.querySelector("#contactError");
+    if(email.match(pattern)){
+        db.collection("queries").add({
+            email:email,
+            name:name,
+            subject:subject
+        })
+        .then(function (res) {
+            contactForm.reset()
+        })
+        .catch(function (err) {
+            contactError.innerHTML=err.message;
+        }); 
+    }
 
-        else{
-        
-            contactError.innerHTML="please enter valid email address";
-            e.preventDefault();
-        }
+    else{
+        contactError.innerHTML="please enter valid email address";
+    }
        
-    })
+})
 
-}
 
-function commentValidation(){
+/*function commentValidation(){
     const email= document.getElementById('email').value;
     const form= document.getElementById('form');
     const commentError=document.getElementById('commentError');
@@ -72,3 +67,4 @@ function commentValidation(){
     })
 
 }
+*/
