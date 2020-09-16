b0ad56079eb9f9f9ec46d8ce850def9bc34c064a
mugabodeo <mugabodeo6@gmail.com>
2020-09-16 18:55:30 +0200

    fixed deployment

--- app-compiled.js
+++ app-compiled.js
@@ -5,32 +5,45 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 
-var _app = _interopRequireDefault(require("./app"));
+var _express = _interopRequireDefault(require("express"));
 
 var _mongoose = _interopRequireDefault(require("mongoose"));
 
+var _articles = _interopRequireDefault(require("./routes/articles"));
+
+var _queries = _interopRequireDefault(require("./routes/queries"));
+
+var _userProfile = _interopRequireDefault(require("./routes/userProfile"));
+
+var _admin = _interopRequireDefault(require("./routes/admin"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var port = process.env.PORT || 8080; //connecting database
+require('dotenv').config();
 
-var connectDb = function connectDb() {
-  return new Promise(function (resolve, reject) {
-    _mongoose["default"].connect(process.env.DB_CONNECTION, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }).then(function (res, err) {
-      if (err) return reject(err);
-      resolve();
-      console.log('connected to db');
-    });
-  });
-}; //creating a server
+var app = (0, _express["default"])();
+var port = process.env.PORT || 8080;
+app.use(_express["default"].json()); //defining routes
+
+app.use('/', _userProfile["default"]);
+app.use('/articles', _articles["default"]);
+app.use('/queries', _queries["default"]);
+app.use('/admin', _admin["default"]); //connect to database
+
+_mongoose["default"].connect(process.env.DB_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(function (res) {
+  console.log('database connnected');
+})["catch"](function (err) {
+  console.log('failed to connect to database', err);
+}); //starting a server
 
 
-connectDb().then(function () {
-  _app["default"].listen(port, function () {
-    console.log(" listening at ".concat(port));
-  });
+var _default = app.listen(port, function () {
+  console.log(" listening at ".concat(port));
 });
-var _default = connectDb;
+
 exports["default"] = _default;
